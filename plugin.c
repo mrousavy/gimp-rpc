67c6bc10a17dfaaa48e8f6e4c69b4004168553e4
Marc Rousavy <marcrousavy@hotmail.com>
2019-03-25 14:34:16 +0100

    Create plugin.c

---
+++ plugin.c
@@ -0,0 +1,84 @@
+#include <libgimp/gimp.h>
+
+static void query (void);
+static void run   (const gchar      *name,
+                   gint              nparams,
+                   const GimpParam  *param,
+                   gint             *nreturn_vals,
+                   GimpParam       **return_vals);
+
+GimpPlugInInfo PLUG_IN_INFO =
+{
+  NULL,
+  NULL,
+  query,
+  run
+};
+
+MAIN()
+
+static void
+query (void)
+{
+  static GimpParamDef args[] =
+  {
+    {
+      GIMP_PDB_INT32,
+      "run-mode",
+      "Run mode"
+    },
+    {
+      GIMP_PDB_IMAGE,
+      "image",
+      "Input image"
+    },
+    {
+      GIMP_PDB_DRAWABLE,
+      "drawable",
+      "Input drawable"
+    }
+  };
+
+  gimp_install_procedure (
+    "plug-in-hello",
+    "Hello, world!",
+    "Displays \"Hello, world!\" in a dialog",
+    "David Neary",
+    "Copyright David Neary",
+    "2004",
+    "_Hello world...",
+    "RGB*, GRAY*",
+    GIMP_PLUGIN,
+    G_N_ELEMENTS (args), 0,
+    args, NULL);
+
+  gimp_plugin_menu_register ("plug-in-hello",
+                             "<Image>/Filters/Misc");
+}
+
+static void
+run (const gchar      *name,
+     gint              nparams,
+     const GimpParam  *param,
+     gint             *nreturn_vals,
+     GimpParam       **return_vals)
+{
+  static GimpParam  values[1];
+  GimpPDBStatusType status = GIMP_PDB_SUCCESS;
+  GimpRunMode       run_mode;
+
+  /* Setting mandatory output values */
+  *nreturn_vals = 1;
+  *return_vals  = values;
+
+  values[0].type = GIMP_PDB_STATUS;
+  values[0].data.d_status = status;
+
+  /* Getting run_mode - we won't display a dialog if
+   * we are in NONINTERACTIVE mode */
+  run_mode = param[0].data.d_int32;
+
+  if (run_mode != GIMP_RUN_NONINTERACTIVE)
+    g_message("Hello, world!\n");
+}
+
